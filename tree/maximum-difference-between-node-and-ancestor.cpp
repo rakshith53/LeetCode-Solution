376a2b865fee8b4eef806ccc9adb40fba8d5a47f
rakshith53 <44432764+rakshith53@users.noreply.github.com>
2021-01-05 04:25:07 +0530

    Create maximum-difference-between-node-and-ancestor.cpp

---
+++ tree/maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root,int mn=INT_MAX,int mx=INT_MIN) {
+        if(!root) return mx-mn;
+        mn = min(mn,root->val);
+        mx = max(mx,root->val);
+        return max(maxAncestorDiff(root->left,mn,mx),maxAncestorDiff(root->right,mn,mx));
+    }
+};
