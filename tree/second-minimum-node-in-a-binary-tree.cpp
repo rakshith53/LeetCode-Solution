e3a1c0e696f99935928b8044fe7800f71a9bf7cd
rakshith53 <44432764+rakshith53@users.noreply.github.com>
2021-01-05 04:31:04 +0530

    Create second-minimum-node-in-a-binary-tree.cpp

---
+++ tree/second-minimum-node-in-a-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    set<int>S;
+    void secMin(TreeNode *root){
+        if(root==NULL) return;
+        S.insert(root->val);
+        secMin(root->left);
+        secMin(root->right);
+    }
+    
+    int findSecondMinimumValue(TreeNode* root) {
+        secMin(root);
+        if(S.size()>=2){
+            int x = *next(S.begin(), 1);
+            return x;
+        }
+        return -1;
+    }
+};
