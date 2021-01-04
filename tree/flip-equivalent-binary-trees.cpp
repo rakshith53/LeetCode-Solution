5c28a5eb4d3313580f45d46bed618129871144ee
rakshith53 <44432764+rakshith53@users.noreply.github.com>
2021-01-05 04:22:44 +0530

    Create flip-equivalent-binary-trees.cpp

---
+++ tree/flip-equivalent-binary-trees.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* r1, TreeNode* r2) {
+        if(!r1 && !r2) return true;
+        if(!r1 || !r2 ) return false;
+        return (r1->val==r2->val && (flipEquiv(r1->left,r2->right) && flipEquiv(r1->right,r2->left)) || (flipEquiv(r1->left,r2->left) && flipEquiv(r1->right,r2->right)));
+    }
+};
