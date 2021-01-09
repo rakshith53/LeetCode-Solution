a53c25f9a723439f5a5827b7536c4f8984f2aff8
rakshith53 <44432764+rakshith53@users.noreply.github.com>
2021-01-09 09:27:03 +0530

    Create sqrtx.cpp

---
+++ binary-search/sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<=1) return x;
+        int l = 1,r =x/2,mid;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(mid == x/mid) return mid;
+            else if(mid < x/mid) l = mid+1;
+            else r = mid-1;
+        }
+        return l-1;
+    }
+};
