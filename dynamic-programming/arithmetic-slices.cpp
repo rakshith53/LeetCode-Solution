1ba8674a47975b3977128ae0b7c91f18f3349fe7
rakshith53 <44432764+rakshith53@users.noreply.github.com>
2021-01-13 13:27:36 +0530

    Create arithmetic-slices.cpp

---
+++ dynamic-programming/arithmetic-slices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& a) {
+        int n = a.size(),dp=0,ans=0;
+        for(int i=2;i<n;i++){
+            if(a[i]-a[i-1] == a[i-1]-a[i-2]){
+                dp++;
+                ans+=dp;
+            }
+            else
+                dp=0;
+        }
+        return ans;
+    }
+};
